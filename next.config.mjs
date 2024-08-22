0c291b5aea5d50beb7b974dca44eb884b02304f9
unknown <nghiatbag8888@gmail.com>
2024-08-22 12:13:13 +0700

    frontend_22_8_6

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
         return [
             {
                 source: '/:path*',
-                destination: process.env.NEXT_PUBLIC_API_URL_UPLOAD + "/:path*",
+                destination: process.env.NEXT_PUBLIC_API_URL_UPLOAD + ":path*",
             }
         ]
     },
