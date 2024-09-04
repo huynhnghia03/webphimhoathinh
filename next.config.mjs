bc71c40b814cc8ebcb39915e144cba44f904c2eb
LAPTOP-ISTJJHOO\Huynh Huu Nghia <nghiatbag8888@gmail.com>
2024-09-04 22:05:24 +0700

    9_4_4

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     // output: 'export',
     // output: 'standalone',
+    siteUrl: process.env.NEXT_PUBLIC_API_FRONTEND,
+    generateRobotsTxt: true, // (optional) create robots.txt file
+    sitemapSize: 7000, // số lượng tối đa các URL trong một sitemap
     images: {
         remotePatterns: [
             {
