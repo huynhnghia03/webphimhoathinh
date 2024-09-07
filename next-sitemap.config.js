befc90f846300165b7f034c1531edef864f967c0
LAPTOP-ISTJJHOO\Huynh Huu Nghia <nghiatbag8888@gmail.com>
2024-09-07 19:28:07 +0700

    9_7_1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     // output: 'export',
     // output: 'standalone',
-    siteUrl: process.env.NEXT_PUBLIC_API_FRONTEND,
+    siteUrl: process.env.NEXT_PUBLIC_API_FRONTEND || "https://hoathinhtq3d.xyz/",
     generateRobotsTxt: true, // (optional) create robots.txt file
     sitemapSize: 7000, // số lượng tối đa các URL trong một sitemap
 }
